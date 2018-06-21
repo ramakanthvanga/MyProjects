69a4fc470dc115e29b6f3feead7e3579b64b439e
Ramakanth Vanga <vanga011@d.umn.edu>
2018-06-20 18:25:02 -0700

    Updated code for charge account predictions.

---
+++ ML/capstone_project/AutCoding.py
@@ -0,0 +1,32 @@
+from pandas import DataFrame, read_csv
+from sklearn.naive_bayes import GaussianNB
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score,fbeta_score
+from sklearn.cross_validation import train_test_split
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+###
+data=pd.read_csv('auto_coding_extract_2.csv', sep=',',verbose = False,float_precision=None,dtype = str)
+data=data.fillna(value='1',axis=1)
+features=pd.DataFrame(data)
+mask = features['VENDOR_ID'] == '462'
+features = features[mask]
+le_dict = {col: LabelEncoder() for col in data.columns }
+
+#Vendor Id-  23348-100%, 462-68%, 959-31.49%, 60650-26.26%, 629-18.67%, 5628-68.52%
+data=features
+for col in data.columns:
+  features[col]=le_dict[col].fit_transform(data[col])
+
+coding = features['CHARGE_ACCOUNT_ID']
+##Store only features by dropping final value
+features = features.drop('CHARGE_ACCOUNT_ID', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(features, coding, test_size = 0.2, random_state = 12)
+model = GaussianNB()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+model_score = accuracy_score(y_test, predictions)
+
